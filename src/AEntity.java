7cc7436be1ccb8820f9ccadc225a9b0144f48e5a
Albertan <albertan@bu.edu>
2021-10-30 00:26:59 -0400

    modify

--- src/AEntity.java
+++ src/AEntity.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import jdk.nashorn.internal.objects.NativeError;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,21 +8,18 @@ public class AEntity {
     private int windowSize;
     private int tempSeqNum; // the sequence number that
     private int windowStartNum;   // this value is also equal to the first unAcked sequence number
-    private boolean waitingForSlide;
     private Checksum checksum;
     private HashMap<Integer, Packet> buffer;  // buffer all the unAcked packets that generated from received messages
     private HashMap<Integer, Packet> bufferForSend;  // buffer all the packets that generated from received messages but are not sent yet.
-    private ArrayList<Boolean> window;  // record the Ack state of each packet in window
+//    private ArrayList<Boolean> window;  // record the Ack state of each packet in window
 
     public AEntity(int windowSize) {
         this.windowSize = windowSize;
         this.tempSeqNum = 0;
         this.windowStartNum = 0;
-        this.waitingForSlide = false;
         this.checksum = new Checksum();
         this.buffer = new HashMap<>();
         this.bufferForSend = new HashMap<>();
-        this.window = new ArrayList<>();
     }
 
 
@@ -40,11 +38,10 @@ public class AEntity {
     public void output(Message message) {
         Packet packet = new Packet(tempSeqNum, -1, checksum.calculateChecksum(tempSeqNum, -1, message.getData()));
         buffer.put(tempSeqNum, packet);
-        window.add();
-        if(!waitingForSlide) {
-            NetworkSimulator.toLayer3(0, packet);
+        if(!isWaiting(tempSeqNum)) {
+//            window.add(false);
+            NetworkSimulator.toLayer3(0, packet); // send the packet to layer3 and transfer
             NetworkSimulator.startTimer(0, 20);
-            if(window.size() == windowSize) waitingForSlide = true;
         } else {
             bufferForSend.put(tempSeqNum, packet);
         }
@@ -59,46 +56,22 @@ public class AEntity {
      * @param packet: The packet that got from the layer3 medium
      */
     public void input(Packet packet) {
-        int checkSum = checksum.calculateChecksum(packet.getSeqnum(), packet.getAcknum(), packet.getPayload());
+        int checkSum = checksum.calculateChecksum(packet);
         Packet firstUnAckedPacket = null;
         boolean flag = true;
         if(checkSum == packet.getChecksum()) {
-            for (Pair<Integer, Boolean> pair : window) {
-                if(flag && pair.getValue().equals(false)) {
-                    firstUnAckedPacket = buffer.get(pair.getKey());
-                    flag = false;
-                }
-                if(pair.getKey().equals(packet.getAcknum())) {
-                    int index = window.indexOf(pair);
-                    buffer.remove(packet.getAcknum()); // remove the ACK packet from the buffer
-                    if(pair.getValue()) {   // duplicate ACK
-                        // retransmit the first unAcked packet
-                        NetworkSimulator.toLayer3(0, firstUnAckedPacket);
-                    } else {
-                        NetworkSimulator.stopTimer(0);
-                        // acknowledged the first packet in the window
-                        if(index == 0) {
-                            window.remove(pair);
-                        } else {
-                            window.set(index, new Pair<>(pair.getKey(), true));
-                        }
-                        while(window.size() < windowSize && bufferForSend.size() > 0) {
-                            waitingForSlide = false;
-                            int numberBehindWindow = windowStartNum + window.size();
-                            if (bufferForSend.containsKey(numberBehindWindow)) {
-                                Packet packet1 = bufferForSend.get(numberBehindWindow);
-                                window.add(new Pair<>(numberBehindWindow, false));
-                                NetworkSimulator.toLayer3(0, packet1);
-                                bufferForSend.remove(numberBehindWindow);
-                            }
-                        }
-                    }
-                    break;
-                } else {
-                    // the ack packet is not in the window, which means it is duplicate ack
-                    // retransmit the first unAcked packet
-                    NetworkSimulator.toLayer3(0, firstUnAckedPacket);
-                }
+            int ackedNum = packet.getAcknum();
+            // if the packet is acknowledged, then remove it from the buffer
+            buffer.remove(ackedNum-1);
+            if(ackedNum == windowStartNum) {
+                // this means it is a duplicate ack, retransmit the first unAcked packet, which is windowStartNum
+                Packet retransmitPacket = buffer.get(windowStartNum);
+                NetworkSimulator.toLayer3(0, retransmitPacket);
+                // timer?
+
+            } else {
+                // received the cumulative acknowledgement
+                windowStartNum = ackedNum;
             }
         }
     }
@@ -121,5 +94,9 @@ public class AEntity {
      */
     public void init() {
 
+    }
+
+    private boolean isWaiting(int tempSeqNum) {
+        return tempSeqNum - windowStartNum + 1 == windowSize;
     }
 }
