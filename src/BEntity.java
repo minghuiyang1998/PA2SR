38201fe0f26a0311c6e954481a411302070eab71
Yang Minghui <minghuiyang1998@gmail.com>
2021-10-30 11:33:31 +0800

    [add] B entity process

--- src/BEntity.java
+++ src/BEntity.java
@@ -3,14 +3,14 @@ public class BEntity {
 
     }
 
-    public void init() {
-
-    }
-
+    // called by simulator
     public void input(Packet packet) {
-        // 检查corrupt/
-        // buffer unorder
-
+        // 1. Check if the packet is corrupted and take appropriate actions
+        // 2. If the data packet is new, and in-order, deliver the data to layer5 and send ACK to A. Note
+        //that you might have subsequent data packets waiting in the buffer at B that also need to be
+        //delivered to layer5
+        //3. If the data packet is new, and out of order, buffer the data packet and send an ACK
+        //4. If the data packet is duplicate, drop it and send an ACK
 
     }
 }
